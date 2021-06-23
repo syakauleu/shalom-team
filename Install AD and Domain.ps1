545094832d6a834292210937d804cd69e2ffdd23
syakauleu <83639147+syakauleu@users.noreply.github.com>
2021-06-23 22:59:41 +0300

    Add files via upload

---
+++ Install AD and Domain.ps1
@@ -0,0 +1,32 @@
+﻿# Install AD 
+
+# show roles and features
+Get-WindowsFeature
+
+# Install AD
+
+install-windowsfeature AD-Domain-Services
+
+# Install AD succes
+
+# Install first cotroller 
+
+Import-Module ADDSDeployment
+
+Install-ADDSForest -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012" -DomainName "testdomain.local" -DomainNetbiosName "TESTDOMAIN" -ForestMode "Win2012" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootONCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -force:$true
+
+# Get domain information of the current local computer domain
+
+Get-ADDomain -Current LocalComputer
+
+# Get domain information from Active Directory
+
+Get-ADDomain -Identity testdomain.local
+
+# check connection domain
+
+if ((Test-Connection -ComputerName testdomain.local -Quiet) -eq $true)
+
+{Write-Host "Connection established"}
+
+else {Write-Host "Connection failed"}
